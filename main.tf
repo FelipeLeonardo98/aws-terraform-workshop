8fe3ff785a7b3679ee690f665f249a51869034bd
FelipeLeonardo98 <felipeferrazleonardo98@gmail.com>
2026-02-16 20:39:51 -0300

    FIX: ADJUST parameter name

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "aws_ssm_parameter" "control-environment" {
+resource "aws_ssm_parameter" "control-environment-dev" {
   name = "ssm-control-dev-environment"
   type = "String"
   value = "development"
 }
 
-resource "aws_ssm_parameter" "control-environment" {
+resource "aws_ssm_parameter" "control-environment-hom" {
   name = "ssm-control-hom-environment"
   type = "String"
   value = "homologation"
